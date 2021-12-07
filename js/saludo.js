e1a7b2be7eee1cb328ae0545505942510e4da506
Miguel Angel Ramos <return.infox@gmail.com>
2021-12-07 18:45:46 -0300

    Mi primer programa javascript, uso de variables y condicional if

---
+++ js/saludo.js
@@ -0,0 +1,53 @@
+let nombre;
+let opcion;
+
+nombre = prompt("Hola cual es tu nombre?: ");
+opcion = prompt("Te gustan los gatitos?, escribe 'si' o 'no' "); // si Si SI
+
+console.log( opcion.toLowerCase() == 'si');
+// condición booleana se refiere a dos opciones VERDADERO O FALSO ( TRUE O FALSE)
+
+// no == si es Falso (false)
+// si == si es verdadero (true)
+
+let velocidad = 100; // asignacion = 
+velocidad == 200; // comparación ==
+
+if ( opcion.toLowerCase() == 'si') {
+  // si la condición es verdadera
+  alert('Hola ' + nombre + ' a mi tambien me gustan los gatitos');
+} else {
+  // si la condición es falsa
+  alert('Hola ' + nombre + ' con lo que has escrito, veo que no te gustan los gatitos');
+}
+
+
+// 'c' 'A' 'B' 'Mi nombre'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var, let y const
+// let nombre = "Miguel";
+// nombre = "Andres";
+// const PI = 3.14;
+
